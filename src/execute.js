e344d43aaeef47e21486c8a62d68fcd2d017c8bc
fukuiretu <fukuiretu@gmail.com>
2019-03-10 23:32:00 +0900

    Completion🎉

--- src/execute.js
+++ src/execute.js
@@ -1,13 +1,40 @@
+import sketch from "sketch";
 import dom from "sketch/dom";
+import fs from "@skpm/fs";
+
 // documentation: https://developer.sketchapp.com/reference/api/
 
-// export default function() {
-//   sketch.UI.message("It's alive 🙌")
-// }
+const VUE_SKELETON_CODE = `<template>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
+`;
+const DIST_DIR = "/tmp/symbols2vue/components";
 
 export default function(context) {
   const document = dom.getSelectedDocument();
   const symbolsPage = document.pages.find(page => page.name === "Symbols");
   const symboleNames = symbolsPage.layers.map(layer => layer.name);
-  context.document.showMessage(`symboleNames: ${symboleNames}`);
+
+  symboleNames.forEach(symboleName => {
+    const symboleNameSplited = symboleName.split("/");
+    const vueFileName = `${symboleNameSplited.pop()}.vue`;
+    const vueFileDir = `${DIST_DIR}/${symboleNameSplited
+      .join("/")
+      .toLowerCase()}`;
+
+    if (!fs.existsSync(vueFileDir)) {
+      fs.mkdirSync(vueFileDir, { recursive: true });
+    }
+    fs.writeFileSync(`${vueFileDir}/${vueFileName}`, VUE_SKELETON_CODE);
+
+    console.log(`created file: ${vueFileDir}/${vueFileName}`);
+  });
+
+  sketch.UI.message("File output is complete🎉");
 }
